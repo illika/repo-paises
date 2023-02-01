d45daca829706ffae8d18dbfb4bb82982ce93af1
illika <steve.milan7@gmail.com>
2023-02-01 11:58:23 -0500

    Avance Component

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pais } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -17,14 +19,21 @@ export class PorRegionComponent {
   regiones: string[] = ["EU", "EFTA", "CARICOM", "PA", "AU", "USAN", "EEU", "AL", "ASEAN", "CAIS", "CEFTA", "NAFTA", "SAARC"];
   regionActiva: string = "";
 
-  constructor() { }
+  paises: Pais[] = [];
+
+  constructor(private paisService: PaisService) { }
 
   getClassCSS(region: string): string {
     return this.regionActiva === region ? 'btn btn-primary' : 'btn btn-outline-primary'
   }
 
   activarRegion(region: string) {
+    if(region == this.regionActiva) return;
     this.regionActiva = region;
+    this.paises = [];    
+    this.paisService.buscarRegion(region).subscribe((response) => {
+      this.paises = response;
+    })
   }
 
 }
